813f545db95f186730e31bd3ab2711a04d999939
Victoria Chen <taichen86@gmail.com>
2019-02-07 12:46:54 +0100

    state filter

--- main.js
+++ main.js
@@ -1,42 +1,73 @@
 
 var membersTable = document.getElementById( 'members-table' );
-var allMembers = data.results[0].members;
-var partiesSelected = ['R', 'D', 'I'];
+var stateFilter = document.getElementsByName( 'statefilter' )[0];
 
+var allMembers = data.results[0].members;
+var partySelected = ['R', 'D', 'I'];
+var allStates = [];
+var stateSelected = []; // not needed?
 
 function initialize()
 {
+    // initialize states array
+    allMembers.forEach( member =>
+        {
+            if( allStates.indexOf( member.state ) < 0 )
+                allStates.push( member.state );
+        } );
+
+    allStates.forEach( state =>
+        {
+           let option = document.createElement( 'option' );
+           option.setAttribute( 'value', state ); 
+           option.append( document.createTextNode( state ) );
+           stateFilter.append( option );
+        } );
+    
+    stateSelected = allStates;
     insertTableOfMembers();
 }
 
-// filter by party
-function filter()
+function filterByParty()
 {
     var nodes = document.querySelectorAll( ['input[name=partyfilter]:checked'] );
-    partiesSelected = Array.from( nodes ).map( node => node.value );
+    partySelected = Array.from( nodes ).map( node => node.value );
 //    console.log( 'parties selected: ' + partiesSelected );
-    partiesSelected.forEach( party => console.log( party ) );
+    partySelected.forEach( party => console.log( party ) );
     membersTable.innerHTML = '';
-    insertTableOfMembers( partiesSelected );
+    insertTableOfMembers( partySelected, stateSelected );
+}
+
+function filterByState()
+{
+    console.log( 'filter by state: ' + stateFilter.value );
+    membersTable.innerHTML = '';
+ //   stateSelected = stateFilter.value
+    stateSelected = ( stateFilter.value == 'ALL' ) ? allStates : stateFilter.value;
+    insertTableOfMembers( partySelected, stateSelected );
 }
 
 
-function insertTableOfMembers( parties = partiesSelected ) // TODO: refactor!!!
+function insertTableOfMembers( parties = partySelected, states = allStates ) // TODO: refactor!!!
 {
-    /*
-    console.log( data.results[0].members[0] );
-    var numOfMembers = data.results[0].members.length;
-    */
-    console.log( partiesSelected );
-    let members = allMembers.filter( member => partiesSelected.indexOf( member.party ) > -1 );
+    console.log( '==== ' + partySelected + ' || ' + stateSelected );
+    // filter party
+//    console.log( partySelected );
+    let members = allMembers.filter( member => partySelected.indexOf( member.party ) > -1 );
+
+    // filter state
+//    console.log( states );
+    members = members.filter( member => states.indexOf( member.state ) > -1 );
+//    console.log( 'number of members: ' + members.length );
+
     console.log( 'number of members: ' + members.length );
 
 
-    let th = document.createElement('th');
     let tr = document.createElement('tr');
+    let th = document.createElement('th');
     tr.append(th);
     th.append(document.createTextNode('Name'));
-    
+
     th = document.createElement('th');
     th.append(document.createTextNode('Party'));
     tr.append(th);
@@ -53,15 +84,15 @@ function insertTableOfMembers( parties = partiesSelected ) // TODO: refactor!!!
     th.append(document.createTextNode('Percentage'));
     tr.append(th);
 
-
     var thead = document.createElement('thead');
     thead.append(tr);
     membersTable.append(thead);
+    
 
     let tbody = document.createElement('tbody');
     members.forEach( member => {
     
-        //   console.log( member );
+ //       console.log( member );
         let tr = document.createElement('tr');
         let td = document.createElement('td');
         let middlename = member.middle_name? (' ' + member.middle_name + ' ') : ' ';
@@ -97,39 +128,4 @@ function insertTableOfMembers( parties = partiesSelected ) // TODO: refactor!!!
     membersTable.append(tbody);
     
     
-}
-
-function createTHead()
-{
-
-    let th = document.createElement('th');
-    let tr = document.createElement('tr');
-    let tx = document.createTextNode('Name');
-    th.append(tx);
-    tr.append(th);
-    
-    th = document.createElement('th');
-    tx = document.createTextNode('Party');
-    th.append(tx);
-    tr.append(th);
-    
-    th = document.createElement('th');
-    tx = document.createTextNode('State');
-    th.append(tx);
-    tr.append(th);
-    
-    th = document.createElement('th');
-    tx = document.createTextNode('Seniority');
-    th.append(tx);
-    tr.append(th);
-    
-    th = document.createElement('th');
-    tx = document.createTextNode('Percentage');
-    th.append(tx);
-    tr.append(th);
-    
-    var thead = document.createElement('thead');
-    thead.append(tr);
-    membersTable.append(thead);
-
 }
